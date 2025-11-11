5bae90a8eac4111f78b7e59f33781d3d491bd3fa
MyungJin <audwls239@naver.com>
2025-11-11 20:16:29 +0900

    AiPedia backend init

---
+++ backEnd/AiPedia/src/main/java/team/goodmorning/aipedia/AiPediaApplication.java
@@ -0,0 +1,13 @@
+package team.goodmorning.aipedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiPediaApplication.class, args);
+    }
+
+}
